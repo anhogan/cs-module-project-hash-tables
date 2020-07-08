57d898b68c9f1e444cdf340ba96b4227eeeaa4b4
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-07-08 11:58:03 -0400

    No dups solution

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,24 @@
 def no_dups(s):
-    # Your code here
+    if s == "":
+        return ""
 
+    # Set a hash to keep track of the words
+    word_hash = {}
 
+    sArr = s.split(" ")
+
+    # Go through sArr and check each word
+    for word in sArr:
+        if word.lower() in word_hash:
+            word_hash[word.lower()] += 1
+        else:
+            word_hash[word.lower()] = 1
+
+    # Take each key in the hash and add it to an array
+    single_string = list(word_hash.keys())
+    single_string = ' '.join(single_string)
+
+    return single_string
 
 if __name__ == "__main__":
     print(no_dups(""))
